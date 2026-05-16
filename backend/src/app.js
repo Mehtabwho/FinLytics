1757d173273a63464adf16182af7090e368c0627
Mehtabwho <moumitamehtab@gmail.com>
2026-05-16 20:52:32 +0600

    redeploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['https://finlyticsai.vercel.app/'], // Replace with your Vercel frontend URL
+  origin: ['https://finlyticsai.vercel.app'], // Replace with your Vercel frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, // Allow cookies if needed
 }));
